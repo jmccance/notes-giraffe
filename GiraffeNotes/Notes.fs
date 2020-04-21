e88291f520c826e3d13d580358225339fc449c24
Joel McCance <joel@kettle.ai>
2020-04-21 14:56:11 -0500

    Initial database access

--- GiraffeNotes/Notes.fs
+++ GiraffeNotes/Notes.fs
@@ -10,24 +10,46 @@ type Note =
       CreatedAt: DateTime }
 
 module Database =
-    open Dapper
+    open FSharp.Data.Sql
 
-    
+    type private Sql =
+        SqlDataProvider<Common.DatabaseProviderTypes.POSTGRESQL,
+                        "Host=localhost;Database=example;Username=user;Password=password",
+                        CaseSensitivityChange=Common.CaseSensitivityChange.ORIGINAL>
 
-module Web = 
+    type private NoteEntity = Sql.dataContext.``public.noteEntity``
+
+    let private ctx = Sql.GetDataContext()
+
+    let private toModel (note: NoteEntity) =
+        { Id = note.Id.ToString()
+          OwnerId = note.OwnerId
+          Text = note.Text
+          CreatedAt = note.CreatedAt }
+
+    module Notes =
+        let getById id =
+            query {
+                for note in ctx.Public.Note do
+                    where (note.Id = id)
+                    select note
+            }
+            |> Seq.map toModel
+            |> Seq.tryHead
+
+module Web =
     type private CreateNoteReq =
         { Text: string }
 
-    let getNote (id: string): HttpHandler =
-        json
-            { Id = id
-              OwnerId = "nobody"
-              Text = "The note is... F#"
-              CreatedAt = DateTime.UtcNow }
-
     let newNote: HttpHandler =
-        bindJson<CreateNoteReq> 
-        |> fun req -> ServerErrors.notImplemented <| text "TODO"
+        bindJson<CreateNoteReq> |> fun req -> ServerErrors.notImplemented <| text "TODO"
+
+    let getNote (id: string): HttpHandler =
+        id
+        |> Guid.Parse
+        |> Database.Notes.getById
+        |> Option.map json
+        |> Option.defaultValue (json "Not found")
 
     let handlers: HttpHandler =
         choose
